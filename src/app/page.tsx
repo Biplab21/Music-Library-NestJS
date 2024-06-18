013881a802e27024a237410cd4984928f2edecb1
biplab21 <biplabpaul569@gmail.com>
2024-06-18 21:14:54 +0530

    music library project completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebiners from "@/components/UpcomingWebiners";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
@@ -9,8 +12,11 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturedCourses/>
-      <WhyChooseUs/>
+      {/* <WhyChooseUs/> */}
       <TestimonialCards/>
+      <UpcomingWebiners/>
+      <Instructors/>
+      <Footer/>
     </main>
   )
 }
